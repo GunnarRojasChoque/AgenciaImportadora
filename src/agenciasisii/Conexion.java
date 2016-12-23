9a62ed9820a9aa278775e98758c2aeef7b67299c
GunnarRojasChoque <gunnarrojas22@gmail.com>
2016-12-23 15:46:49 -0400

    Revert "user history mostrar venta"

--- src/agenciasisii/Conexion.java
+++ src/agenciasisii/Conexion.java
@@ -13,12 +13,8 @@ public class Conexion {
     private String driver="org.postgresql.Driver";
     private String nombreDB="AgenciaImportadora";
     private String url="jdbc:postgresql://localhost:5432/"+nombreDB;
-
-    private String user="singular";
-    private String password="plural";
     private String user="postgres";
     private String password="root";
-
     
     private Connection con;
     Statement st;
