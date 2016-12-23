a2b0bde76f7a6c6653341b0e4d578cf70c37ba39
hp <hp@DESKTOP-D34BB9F>
2016-12-23 12:34:57 +0300

    esttado empleado

---
+++ src/EstadoEmpleado/Empleado.java
@@ -0,0 +1,24 @@
+package EstadoEmpleado;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+class Empleado {
+
+    boolean VerificarCiudad(String ciudad){
+        return false;
+    }
+
+    
+    boolean VerificarPais(String pais) {
+        return true;
+    }
+    
+}
---
+++ src/EstadoEmpleado/FormacionAcademica.java
@@ -0,0 +1,25 @@
+package EstadoEmpleado;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+class FormacionAcademica {
+
+    int getNumCarreras(String nombreEmp) {
+        String nomb1 = "Juan";
+        if( nombreEmp.compareTo(nomb1)==0){
+            return 1;
+        }
+        else{
+            return 5;
+        }
+    }
+    
+}
